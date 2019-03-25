212b68d94d6616e8d67d3386fb73c5538954071e
manukg93 <46193676+manukg93@users.noreply.github.com>
2019-03-25 22:00:13 +0400

    Update MandatoryFieldException.java

--- src/com/egs/customexceptions/MandatoryFieldException.java
+++ src/com/egs/customexceptions/MandatoryFieldException.java
@@ -1,13 +1,14 @@
 package com.egs.customexceptions;
 
 public class MandatoryFieldException extends Exception {
-
+        // So? What is this? 
         private String fieldName;
 
         public MandatoryFieldException(String fieldName) {
             this.fieldName = fieldName;
         }
 
+        // Not good, make a google how to create Custom expection
         @Override
         public String toString() {
             return this.getClass().getName() + "\n" + fieldName + " cannot be null";
