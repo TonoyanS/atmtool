bf0fa5f84fb136cf6d7afa14b28262a9a38078dd
manukg93 <46193676+manukg93@users.noreply.github.com>
2019-03-25 21:55:03 +0400

    Update Parser.java

--- src/com/egs/parser/Parser.java
+++ src/com/egs/parser/Parser.java
@@ -3,6 +3,9 @@ package com.egs.parser;
 import java.text.ParseException;
 import java.util.List;
 
+// Good
+// Would be nice to have @FunctionalInterface annotation
+// What Functional interface is?
 public interface Parser<T> {
 
     List<T> parse (final String path) throws ParseException;
